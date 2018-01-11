e80e4113478ea4037c4d09d98011f149ef6208de
Matt noelker <somexotherxguy@yahoo.com>
2018-01-10 22:48:26 -0600

    Started on js logic for deck editor

--- scripts/controllers/editor.js
+++ scripts/controllers/editor.js
@@ -1,2 +1,35 @@
-app.controller('editor', ['$scope', function($scope) {
+app.controller('editor', ['$scope', '$http', function($scope, $http) {
+    var moves;
+    $http.get('moves.json').
+        then(function onSuccess(data) {
+            moves = data;
+            
+            $scope.fr = [];
+            $scope.fl = [];
+            $scope.bl = [];
+            $scope.br = [];
+            $scope.deck = {"oneA" : "", "oneB" : "", "oneC" : "", "oneD" : "", "twoA" : "", "twoB" : "", "twoC" : "", "twoD" : "", "threeA" : "", "threeB" : "", "threeC" : "", "threeB" : "", "fourA" : "", "fourB" : "", "fourC" : "", "fourD" : ""};
+            for(x in data.data){
+                for(y in data.data[x].stances){
+                    if(data.data[x].stances[y].start == "FR"){
+                        $scope.fr.push(data.data[x]);
+                    } else if(data.data[x].stances[y].start == "FL"){
+                        $scope.fl.push(data.data[x]);
+                    } else if(data.data[x].stances[y].start == "BL"){
+                        $scope.bl.push(data.data[x]);
+                    }else if(data.data[x].stances[y].start == "BR"){
+                        $scope.br.push(data.data[x]);
+                    }
+                }
+            }
+            console.log($scope.fr);
+            console.log($scope.fl);
+            console.log($scope.bl);
+            console.log($scope.br);
+            //defer.resolve();
+            
+        }).
+        catch(function onError(data) {
+            console.log(data);
+        });
 }]);
